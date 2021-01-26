e72742c1600607253700633dd412d56d81891b78
TV-Plagwitz <77416102+TV-Plagwitz@users.noreply.github.com>
2021-01-26 20:29:38 +0100

    Karten

---
+++ tiles/walls.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="walls" tilewidth="32" tileheight="32" tilecount="1152" columns="16">
+ <image source="walls.png" trans="ffffff" width="514" height="2304"/>
+ <tile id="353">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
